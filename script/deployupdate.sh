f5e8fe020902dcd098a53e9a7dffca9bf37138bf
Lee Earley <learley@rfx-tech.com>
2016-09-08 17:05:15 -0400

    Deployment script changes in attempt to fix assets

--- script/deployupdate.sh
+++ script/deployupdate.sh
@@ -4,4 +4,5 @@ source ~/secret_token_env.sh
 rvm use ruby-2.1.7
 bundle install --deployment --without development test
 bundle exec rake tmp:cache:clear
-bundle exec rake assets:precompile db:migrate RAILS_ENV="production"
+bundle exec rake assets:clobber RAILS_ENV="production"
+bundle exec rake assets:precompile RAILS_ENV="production"
